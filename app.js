4cf8e5159abed41bdaa7e423c6dd759555d73e75
Jules Perryman <jules.perryman@gmail.com>
2017-02-28 13:46:36 -0700

    updated files

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-$(document).ready(function(){
-    console.log('ready');
+$(document).ready(function() {
+    // console.log('ready');
+    $("button").click(function() {
+        getMovie()
+    })
 
-    $.get("http://www.omdbapi.com/?t=rambo", function(data) {
+    // $.get("http://www.omdbapi.com/?t=rambo", function(data) {
+    //     console.log(data);
+    // });
+
+
+});
+
+var getMovie = function() {
+    var url = "http://www.omdbapi.com/?t="
+    url += $("input").val()
+    console.log(url);
+
+    $.get(url, function(data) {
         console.log(data);
-});
-});
+        var title = data.Title
+        console.log(title, poster);
+        var poster = data.Poster
+        updatePage(title, poster)
+    })
+    // .then(function(){
+    //     showMessage()
+    // })
+
+
+    function updatePage(title,poster) {
+        $(".title").append(title)
+        console.log(poster);
+        $(".poster").append('<img src="" '> + poster)
+    }
+};
